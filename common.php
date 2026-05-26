a6a66bf15992ab0ad9cbd9a71ffe1659c8a1b095
João Paulo <jprzimba@gmail.com>
2026-05-26 10:03:53 -0300

    update version

--- common.php
+++ common.php
@@ -26,7 +26,7 @@ global $config;
 if (version_compare(phpversion(), '7.4', '<')) die('PHP version 7.4 or higher is required.');
 
 define('MYAAC', true);
-define('MYAAC_VERSION', '0.8.28');
+define('MYAAC_VERSION', '0.8.29');
 define('DATABASE_VERSION', 35);
 define('TABLE_PREFIX', 'myaac_');
 define('START_TIME', microtime(true));
